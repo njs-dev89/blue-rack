97ea5ee7f0564b559ec29c7f499a764c39bd3935
Najam us saqib <najamkhan89@gmail.com>
2021-08-01 12:20:37 +0500

    "another"

---
+++ src/pages/resellerHosting.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HostingPageHeader from "../components/HostingPageHeader";
+import Layout from "../components/Layout";
+
+export default function resellerHosting() {
+  return (
+    <Layout>
+      <HostingPageHeader />
+    </Layout>
+  );
+}
